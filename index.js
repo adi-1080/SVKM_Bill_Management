efe5c8d67d764703a486e6e8ff0761953db16e2c
krishnathakkar29 <work.krishnat@gmail.com>
2025-02-14 10:40:21 +0530

    setup init

---
+++ index.js
@@ -0,0 +1,26 @@
+import cookieParser from "cookie-parser";
+import "dotenv/config";
+import express from "express";
+import cors from "cors";
+import { connectDB } from "./utils/db.js";
+
+const app = express();
+
+const port = process.env.PORT || 5000;
+
+connectDB();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(
+  cors({
+    credentials: true,
+    origin: process.env.CLIENT_URL,
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.listen(port, () => {
+  console.log(`server is listening on port ${port}`);
+});
