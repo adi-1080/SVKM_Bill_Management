c37c9dd1a1a6628aee086a6bcbaa07de6c8c30b5
adi-1080 <adityagupta5277@gmail.com>
2025-02-19 16:24:53 +0530

    fix bill workflow

--- index.js
+++ index.js
@@ -24,10 +24,12 @@ app.use(
 import billRoute from "./routes/bill-route.js";
 import userRoute from "./routes/user-route.js";
 import vendorRoute from "./routes/vendor-route.js";
+import billWorkflowRoute from "./routes/bill-workflow-route.js";
 
 app.use('/bill', billRoute);
 app.use('/users', userRoute);
 app.use('/vendors', vendorRoute);
+app.use('/bwf', billWorkflowRoute);
 
 // Swagger docs route
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
