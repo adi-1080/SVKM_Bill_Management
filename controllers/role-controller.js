543cb76cbdfdc626f258e327058fce44f2f6991f
adi-1080 <adityagupta5277@gmail.com>
2025-04-09 17:36:07 +0530

    added all subsequent roles too

--- controllers/role-controller.js
+++ controllers/role-controller.js
@@ -3,13 +3,18 @@ import Role from "../models/role-model.js";
 const createRole = async (req, res) => {
   try {
     const { role, permissions } = req.body;
-    const newRole = new Role({ role, permissions });
+    const existingRole = await Role.findOne({ role: role.trim()});
+    if (existingRole) {
+      return res.status(400).json({ message: "Role already exists" });
+    }
+    const newRole = new Role({ role: role.trim(), permissions });
     await newRole.save();
     res.status(201).json(newRole);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
 };
+
 
 const getRoles = async (req, res) => {
   try {
